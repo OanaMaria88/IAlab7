4f9d5c5a36e877679dc69d90aff827d3924d7e76
Oana Maria Stanciu <stanciu.io.oana@student.utcluj.ro>
2024-11-13 17:54:08 +0200

    exercitii

---
+++ ex01/ex1.js
@@ -0,0 +1,4 @@
+var p=2;
+var g=3;
+var N=4*p+2*g;
+console.log("Nr tottal de picioarev din curte:",N);
---
+++ ex02/ex2.js
@@ -0,0 +1,8 @@
+var n=1234;
+var s=0;
+while(n!=0)
+{
+    s=s+n%10;
+    n/10;
+}
+console.log(s);
